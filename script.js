6eb60fab10319ab63c11a75b7aa2c84b30132238
juanparuizzz <jplr122005@gmail.com>
2026-06-08 17:18:31 -0600

    Commit 1: vista inicial y estilos

---
+++ script.js
@@ -0,0 +1,9 @@
+// Al cargar la página, muestra un mensaje
+window.onload = function() {
+  alert("¡Hola Juan! Tu práctica está lista 🚀");
+};
+
+// Ejemplo: cambiar color de fondo al hacer clic en un botón
+function cambiarColor() {
+  document.body.style.backgroundColor = "#d1f7c4";
+}
