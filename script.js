5e5202610c243cb1a67f34562baabb172451f026
juanparuizzz <jplr122005@gmail.com>
2026-06-08 17:44:23 -0600

    Commit 3: funcionalidad básica con JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Al cargar la página, muestra un mensaje
 window.onload = function() {
-  alert("¡Hola Juan! Tu práctica está lista 🚀");
+  alert("¡Hola Juan! Tu práctica está lista ");
 };
 
 // Ejemplo: cambiar color de fondo al hacer clic en un botón
